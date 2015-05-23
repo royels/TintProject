0acfc03e1d308291922cec5a63c658c3605abf93
royels <docmentz@gmail.com>
2015-05-23 01:58:00 -0700

    I hope this works

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   def display
-	  @value = params[:tweet][:keyword] 	  
+	  @value = params[:tweet][:keyword]
+	  @tweets = $twitter.search(@value, result_type: "recent").collect
   end
 end
