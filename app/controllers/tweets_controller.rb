64e91d123a4eacbc334dd14c17cc9dc295d7d165
royels <docmentz@gmail.com>
2015-05-22 20:24:19 -0700

    I think i got twitter to work

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   def display
 	  @value = params[:tweet][:keyword] 
+	  $twitter.update("I finally got the twitter api to work!....If I can read this")
   end
 end
