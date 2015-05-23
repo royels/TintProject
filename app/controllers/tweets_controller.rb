ce28442f660a890b10d938f1a560c41b4e462d99
royels <docmentz@gmail.com>
2015-05-22 20:33:35 -0700

    further testing twitter

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   def display
 	  @value = params[:tweet][:keyword] 
-	  $twitter.update("I finally got the twitter api to work!....If I can read this")
+	  $twitter.update(@value)
   end
 end
