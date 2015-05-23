c19a92026287489de2ef9028c40f4d7e6dfd7325
royels <docmentz@gmail.com>
2015-05-23 10:37:21 -0700

    got routing redirects to work

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 	resources :tweets
   	root                'static#form'
 	post 'display'    => 'tweets#display'
-	get  'home'		  => 'static#form'
+	get  'display', to: redirect('/')
+	get  'home' => 'static#form'
 end
