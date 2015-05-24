e3d2c134c8112eb86b4cfed2ed77deb8207fd327
royels <docmentz@gmail.com>
2015-05-23 22:09:47 -0700

    Derp

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,12 @@
 class TweetsController < ApplicationController
   def display
 	  @value = params[:tweet][:keyword]
-	  @tweet = $twitter.search(@value, result_type: "recent").take(1).collect
-	  @tweetval = @tweet.next
-	  @tweetuser = @tweetval.user.screen_name
-	  @tweetcountry = @tweetval.user.location
-	  @tweettext = @tweetval.text
-	  @sentiment = Sentimentalizer.analyze(@tweettext)['text']
+	  $twitter.search(@value, result_type: "recent").take(1).collect do |tweet|
+		  @tweetuser = tweet.user.screen_name
+		  @tweetcountry = tweet.user.location
+		  @tweettext = tweet.text
+	  	  @sentiment = Sentimentalizer.analyze(@tweettext)['text']	  
+	  end
 	  gon.val = 3
   end
 end
