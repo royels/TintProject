47ee18fa8f630741e0d3d5080711e1c3e7d0ce06
royels <docmentz@gmail.com>
2015-05-23 21:12:00 -0700

    nothing left to say

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,10 @@ class TweetsController < ApplicationController
   def display
 	  @value = params[:tweet][:keyword]
 	  @tweets = $twitter.search(@value, result_type: "recent").take(1).collect
+	  @tweetval = @tweets.next
+	  
+	  
+	  
 	  gon.val = 3
   end
 end
