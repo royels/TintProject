de1f37961b23c306c3713be966be3944063c40bf
royels <docmentz@gmail.com>
2015-05-23 20:57:30 -0700

    testing something

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
   def display
 	  @value = params[:tweet][:keyword]
-#	  @tweets = $twitter.search(@value, result_type: "recent").take(3).collect
+	  @tweets = $twitter.search(@value, result_type: "recent").take(1).collect
 	  gon.val = 3
   end
 end
