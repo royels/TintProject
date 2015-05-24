f260578d70729a198dab54f839bc7644072be1f3
royels <docmentz@gmail.com>
2015-05-23 21:31:44 -0700

    I would really like this to work

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,9 +3,10 @@ class TweetsController < ApplicationController
 	  @value = params[:tweet][:keyword]
 	  @tweets = $twitter.search(@value, result_type: "recent").take(1).collect
 	  @tweetval = @tweets.next
-	  
-	  
-	  
+	  @tweetuser = @tweetval.user.screen_name
+	  @tweetcountry1 = @tweetval.user.location
+	  @tweetcountry2 = @tweetval.place.country
+	  @tweettext = @tweetval.text
 	  gon.val = 3
   end
 end
