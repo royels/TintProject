442cdfe404d48ceb8e94084e0c5abf3dc54ade58
royels <docmentz@gmail.com>
2015-05-30 00:02:25 -0700

    i think this works

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,18 +1,13 @@
 class TweetsController < ApplicationController
   def display
-	  @value = params[:tweet][:keyword]
-	  $twitter.search(@value, result_type: "recent").take(30).collect do |tweet|
+	  value = params[:tweet][:keyword]
+	  $twitter.search(value, result_type: "recent").take(30).collect do |tweet|
 		  tweetuser = tweet.user.screen_name
 		  tweetcountry = tweet.user.location
 		  tweettext = tweet.text
-		  sentiment = Sentimentalizer.analyze(@tweettext).overall_probability
-		  Tweet.create(content: tweettext, user: tweetuser, location: tweetcountry, keyword: @value, sentiment: sentiment)
+		  sentiment = Sentimentalizer.analyze(tweettext).overall_probability
+		  Tweet.create(content: tweettext, user: tweetuser, location: tweetcountry, keyword: value, sentiment: sentiment)
 	  end
+	  @client = Tweet.where("keyword = ?", params[:tweet][:keyword])
   end
-	
-	private
-	
-	
-	def get_relevant_tweets
-		client = Tweet.find_by keyword: @value
 end
